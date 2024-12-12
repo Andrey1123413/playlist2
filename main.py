ae86c0b23076f78c2b883ba1676dd68e81d67ba4
Andrey1123413 <dimasous620@gmail.com>
2024-12-12 14:16:51 +0300

    Добавил плейлисты

--- main.py
+++ main.py
@@ -34,3 +34,22 @@ def get_duration(playlist_e, playlist_f, n):
     selected_songs = select_random_songs(combined_playlist, n)
     total_time = calculate_total_time(selected_songs)
     return total_time
+
+# Плейлисты из нашего задания(5 задание 2 кейс Крупкин и Коршунов)
+playlist_e = """
+Sunday 5:09
+Why Does My Heart Feel so Bad? 4.23
+Everlong 3.25
+To Let Myself Go
+Golden 2.56
+Daisuke 2.41
+Miami 3.31
+Chill Bill Lofi 2.05
+The Perfect Girl 1.48
+Resonance 3.32
+"""
+
+playlist_f = (
+    {"Free Bird": 9.08, "Enter Sandman": 5.31, "One": 7.45, "Sliver" : 2.10, "Come as You Are": 3.45},
+    {"Thunderstruck": 4.53, "You Shook Me All Night Long": 3.29, "Everlong" : 4.51, "My Hero" : 4.02},
+)
