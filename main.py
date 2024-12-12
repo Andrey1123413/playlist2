e96ac4b5e826aac3ed50602ddd754759976657fc
Krupkin Maksim <krupkinmacsim@yandex.ru>
2024-12-12 13:54:32 +0300

    Добавил функцию для парсинга плейлиста из кортежа

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def parse_playlist_e(playlist):
     pattern = re.compile(r"(.+?)\s+(\d+[\.:]\d+)")
     songs = pattern.findall(playlist)
     return {name: float(time.replace(':', '.')) for name, time in songs if time}
+# Функция для парсинга плейлиста из кортежа 
+def parse_playlist_f(playlist):
+    combined_dict = {}
+    for p in playlist:
+        combined_dict.update(p)
+    return combined_dict
